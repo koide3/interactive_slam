b9ef8a13a64096d5c50bfd295553e82a087eef98
k.koide <k.koide@aist.go.jp>
2019-10-30 21:37:39 +0900

    add version info

--- src/hdl_graph_slam/version_modal.cpp
+++ src/hdl_graph_slam/version_modal.cpp
@@ -1,3 +1,4 @@
+#include <hdl_graph_slam/version.h>
 #include <hdl_graph_slam/version_modal.hpp>
 
 namespace hdl_graph_slam {
@@ -15,7 +16,9 @@ bool VersionModal::run() {
   ImGuiWindowFlags flags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoTitleBar;
   if(ImGui::BeginPopupModal("Interactive SLAM", nullptr, flags)) {
     ImGui::Text("Interactive SLAM");
-    ImGui::Text("Version 0.0.1");
+    ImGui::Text("Version: %d.%d.%d", IS_VERSION_MAJOR, IS_VERSION_MINOR, IS_VERSION_REVISION);
+    ImGui::Text("Commit hash: %s", LAST_COMMIT_HASH);
+    ImGui::Text("Commit data: %s", LAST_COMMIT_DATE);
     ImGui::Text("\nK.Koide (k.koide@aist.go.jp)");
     ImGui::Text("Smart Mobility Research Team");
     ImGui::Text("AIST, Japan");
